452a5dfacabf1fb39c59d5227a6cbfac78eef27c
DOA-3231230030 <140550655+DOA-3231230030@users.noreply.github.com>
2023-07-31 17:38:48 +0000

    Add files via upload

---
+++ DOA.py
@@ -0,0 +1,32 @@
+# File: DOA.py
+# Description: Print out my initials
+# Assignment Number:1
+#
+# Name: Desmond Ofori Asare
+# Stundent ID: 3231230030
+# Email: 3231230030@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Desmond Ofori Asare, this programing assignment is my own work
+# And I have not provided this code to any other student.
+
+
+def main():
+    # Printing out my initials in style.
+    print('...DOA')
+    print()
+    print('...DDDDDDD.................OOOO................AAA..........')
+    print('...DDDDDDDDD............OOO....OOO............AAAAA.........')
+    print('...DD......DDD.........OOO......OOO..........AAA..AAA.......')
+    print('...DD........DD........OO........OO.........AAA....AAA......')
+    print('...DD........DD........OO........OO........AAAAAAAAAAAA.....')
+    print('...DD........DD........OO........OO........AAAAAAAAAAAA.....')
+    print('...DD........DD........OO........OO........AAAA....AAAA.....')
+    print('...DD.......DDD........OO........OO........AAA......AAA.....')
+    print('...DDDDDDDDDD.....**...OOO......OOO...**...AAA......AAA...**')
+    print('...DDDDDDDD.......**......OOOOOO......**...AAA......AAA...**')
+
+
+main()
+
+
